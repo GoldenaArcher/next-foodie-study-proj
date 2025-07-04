2f24ab5f2f6a279fa12cf00037c51121abfef22e
GoldenaArcher <25376645+GoldenaArcher@users.noreply.github.com>
2025-07-04 03:54:44 -0400

    feat(meals): add meals page with header and styling
    - Create new CSS module for meals page styling
    - Implement header section with title, description and CTA link
    - Add MealsGrid component to display meals (currently empty)

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
-const page = () => {
+const MealsPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          voluptatibus fuga voluptas temporibus porro consequatur totam nihil
+          quae omnis eos blanditiis asperiores, repudiandae itaque officia
+          optio? Repudiandae recusandae sit sequi?
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
+};
 
-export default page
+export default MealsPage;
