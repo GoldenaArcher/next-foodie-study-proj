e1b83d10860cd6e4c333f4f9601c9b9843556518
GoldenaArcher <25376645+GoldenaArcher@users.noreply.github.com>
2025-07-05 19:30:01 -0400

    feat(meals): implement meal details page and improve meals listing
    - Add new meal details page with styling and data fetching
    - Update meals listing page to use suspense for loading state
    - Add getMeal function to fetch single meal by slug

---
+++ app/meals/[mealSlug]/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+
+const page = async ({ params }) => {
+  const meal = await getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill alt={meal.title} src={meal.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default page;
--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import MealsLoadingPage from "./loading";
+
+const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
 
 const MealsPage = () => {
   return (
@@ -22,7 +30,9 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
